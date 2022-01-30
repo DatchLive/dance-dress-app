aa4b91416c0b74b44a78d36711ae967e6e6ac641
datchlive <71320906+DatchLive@users.noreply.github.com>
2022-01-30 23:47:39 +0900

    💄 投稿一覧画面のUI調整(仮)

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -7,18 +7,26 @@
 
   <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <span><a href="{{ route('posts.create')}}">投稿する</a></span>
-          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-              <div class="p-6 bg-white border-b border-gray-200">
-                @foreach($posts as $post)
-                <div>{{$post->title}}</div>
-                <div>カテゴリー名：{{$post->category->category_name}}</div>
-                <div>投稿者：{{$post->user->name}}</div>
-                <div>{{$post->content}}</div>
-                <span class="bg-cyan-600"><a href="{{ route('posts.show', $post->id)}}">ボタン</a></span>
-                @endforeach
-              </div>
+        <div class="mb-6">
+          <span class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <a href="{{ route('posts.create')}}">投稿する</a>
+          </span>
+        </div>
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          @foreach($posts as $post)
+          <div class="p-6 bg-white border-b border-gray-200">
+            <div class="py-4">
+              <div class="font-bold text-xl mb-2">{{$post->title}}</div>
+              <p class="text-gray-700">{{$post->content}}</p>
+            </div>
+            <div class="mb-1 text-base">category:{{$post->category->category_name}}</div>
+            <div class="mb-1 text-base">Contributor:{{$post->user->name}}</div>
+            <span  class="inline-block bg-blue-500 hover:bg-blue-700 text-white rounded px-3 py-1 text-sm font-semibold mr-2 mb-2">
+              <a href="{{ route('posts.show', $post->id)}}">詳細</a>
+            </span>
           </div>
+          @endforeach
+        </div>
       </div>
   </div>
 </x-app-layout>
