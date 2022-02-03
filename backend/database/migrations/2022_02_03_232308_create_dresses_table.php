84985ae943cfdeba85fa2e0d358956c9bdb14580
datchlive <71320906+DatchLive@users.noreply.github.com>
2022-02-04 00:12:28 +0900

    🗃️ dressesテーブルmigration作成

---
+++ backend/database/migrations/2022_02_03_232308_create_dresses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dresses', function (Blueprint $table) {
+            $table->bigIncrements('d_id');
+            $table->string('name', 50);
+            $table->string('genre', 50);
+            $table->string('color', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dresses');
+    }
+}
