b31417a4eedaa004fd315f9426c3e3cf9103386c
datchlive <71320906+DatchLive@users.noreply.github.com>
2022-02-14 23:50:20 +0900

    💄 header・footerデザイン変更

--- backend/resources/views/dress/index.blade.php
+++ backend/resources/views/dress/index.blade.php
@@ -1,10 +1,11 @@
-<h2>ドレス一覧</h2>
+@extends('layouts.app')
 
+@section('content')
 @foreach($dresses as $dress)
 <div class="max-w-sm mb-6 overflow-hidden rounded shadow-lg">
   <img class="w-full" src="https://placehold.jp/200x200.png" alt="Sunset in the mountains">
   <div class="px-6 py-4">
-    <div class="mb-2 text-xl font-bold">{{$dress->name}}</div>
+    <div class="mb-2 text-xl font-bold text-orange-200">{{$dress->name}}</div>
   </div>
   <div class="px-6 pt-4 pb-2">
     <span class="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">{{$dress->color}}</span>
@@ -12,3 +13,4 @@
   <p><a href="{{ route('dress.show', ['d_id'=>$dress->d_id])}}">詳細</a></p>
 </div>
 @endforeach
+@endsection
--- backend/resources/views/dress/show.blade.php
+++ backend/resources/views/dress/show.blade.php
@@ -1,4 +1,7 @@
-<h1>詳細画面</h1>
+@extends('layouts.app')
+
+@section('content')
 <div class="mb-2 text-xl font-bold">{{$dresses->name}}</div>
 <div class="mb-2 text-xl font-bold">{{$dresses->genre}}</div>
 <div class="mb-2 text-xl font-bold">{{$dresses->color}}</div>
+@endsection
